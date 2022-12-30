d5aec70678a1f6a30bab8c37316c1c62ab29efc5
ubuntu-devops <zentkhv27@gmail.com>
2022-12-30 15:10:50 +1000

    HTML page

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 import json
 import datetime
-from flask import Flask
-import random
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -13,9 +12,11 @@ RESOURCE_DIR = os.path.join(BASE_FOLDER, "resources")
 @app.route('/')
 def hello_world():
     with open(os.path.join(RESOURCE_DIR, "response.json")) as f:
-        return f'{json.loads(f.read()).get("payload")} - {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")} || ' \
-               f'Случайное число: {random.randint(1,1000)} '
+        ip = os.popen('curl ifconfig.me')
+        date = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+        version = json.loads(f.read()).get("payload")
+        return render_template('index.html', date=date, version=version, ip=ip.read())
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
