1f9b5876e9bf84f63970ac4176f8ca11ccd6c660
zentkhv <70746340+zentkhv@users.noreply.github.com>
2023-04-29 09:49:02 +1000

    Addd config

---
+++ config.py
@@ -0,0 +1,7 @@
+import datetime
+
+local_address = "0.0.0.0"
+local_port = 80
+
+# Дата и время
+now_date_time = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import json
 import datetime
 from flask import Flask, render_template
 
+import config
+
 app = Flask(__name__)
 
 BASE_FOLDER = os.path.dirname(os.path.abspath(__file__))
@@ -10,13 +12,12 @@ RESOURCE_DIR = os.path.join(BASE_FOLDER, "resources")
 
 
 @app.route('/')
-def hello_world():
+def home():
     with open(os.path.join(RESOURCE_DIR, "response.json")) as f:
         ip = os.popen('hostname -I')
-        date = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
         version = json.loads(f.read()).get("payload")
-        return render_template('index.html', date=date, version=version, ip=ip.read())
+        return render_template('index.html', date=config.now_date_time, version=version, ip=ip.read())
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host=config.local_address, port=config.local_port, debug=True)
